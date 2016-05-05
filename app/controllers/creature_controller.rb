abab3f109a5de7a93dbdfcf60eb03f14f57351af
bengus101 <bgustafson29@yahoo.com>
2016-05-04 22:53:48 -0700

    Filled out creature controller.

--- app/controllers/creature_controller.rb
+++ app/controllers/creature_controller.rb
@@ -1,14 +1,19 @@
 class CreatureController < ApplicationController
   def index
+    @creatures = Creature.all
   end
 
   def new
+    @creature = Creature.new
   end
 
   def create
+    creature = Creature.create creature_params
+    redirect_to main
   end
 
   def show
+    @creature = Creature.find params[:id]
   end
 
   def edit
@@ -19,4 +24,7 @@ class CreatureController < ApplicationController
 
   def destroy
   end
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
 end
