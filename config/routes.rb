ac8fb7801abd6fff427d05eacaaf8b90c9ad6d71
bengus101 <bgustafson29@yahoo.com>
2016-05-04 22:11:25 -0700

    Un-installed then re-installed controllers. I didn't do it right the first time before.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  root 'main#index'
+
+  # get 'creature/index'
+
+  # get 'creature/new'
+
+  # get 'creature/create'
+
+  # get 'creature/show'
+
+  # get 'creature/edit'
+
+  # get 'creature/update'
+
+  # get 'creature/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +69,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :creatures
+  
 end
